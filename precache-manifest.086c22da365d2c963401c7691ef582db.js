02171f4704cff394e10eb7790a8a0670962f583b
Sarath2013 <sarathkumarpitb2013@gmail.com>
2020-01-09 01:05:13 +0530

    Updates

---
+++ precache-manifest.086c22da365d2c963401c7691ef582db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1b2550605250dac41d52bb47761edf",
+    "url": "/GridSquares/index.html"
+  },
+  {
+    "revision": "8d6a5d93efff0718634a",
+    "url": "/GridSquares/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "76b44d887da72f53bcc4",
+    "url": "/GridSquares/static/css/main.dbe68db2.chunk.css"
+  },
+  {
+    "revision": "8d6a5d93efff0718634a",
+    "url": "/GridSquares/static/js/2.b4e63540.chunk.js"
+  },
+  {
+    "revision": "76b44d887da72f53bcc4",
+    "url": "/GridSquares/static/js/main.d3a1c7fc.chunk.js"
+  },
+  {
+    "revision": "6509bbb412c83e7d81fb",
+    "url": "/GridSquares/static/js/runtime-main.0fdb86b5.js"
+  }
+]);
